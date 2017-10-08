3c7d3f8375b2e1438cb55709e93402f20ce63d92
MMMMMua <969765303@qq.com>
2017-10-08 13:27:51 +0800

    fixed bug on template/ST_table.cpp

--- template/ST_table.cpp
+++ template/ST_table.cpp
@@ -2,7 +2,7 @@ void init_rmq(){
 	lg[0] = -1;
 	for(int i = 1; i <= n; ++i) g[i][0] = a[i], lg[i] = lg[i/2] + 1;
 	for(int j = 1; j <= 20; ++j)
-		for(int i = 1; i+(1<<j) < n; ++i)
+		for(int i = 1; i+(1<<j)-1 <= n; ++i)
 			g[i][j] = min(g[i][j-1], g[i+(1<<(j-1))][j-1]);	
 }
 int rmq(int l, int r){
